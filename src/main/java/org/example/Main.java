828383c3b853d283ca5bb6026f609702ed80c276
andywangdream <Wang200902@outlook.com>
2024-04-04 23:13:45 +0800

    PrimeMc Plugin Dirtshop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,7 @@
 package org.example;
 import Command.MainCommand;
-import Command.gonggao;
-import Command.restartServer;
-import com.avaje.ebean.annotation.Sql;
-import com.mysql.jdbc.MySQLConnection;
-import net.minecraft.server.v1_8_R3.CommandHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -17,7 +11,5 @@ public class Main extends JavaPlugin {
         System.out.println("DirtShop By PRIMEMC Loaded");
         getServer().broadcastMessage(ChatColor.RED + "此插件由PRIMEMC编写，如果你在别的服务器看到了这个插件，那么请来PRMC.XYZ游玩正版有更新的纯正泥土商店！");
         Bukkit.getPluginCommand("DirtShop").setExecutor((new MainCommand()));
-        Bukkit.getPluginCommand("gonggao").setExecutor(new gonggao());
-        Bukkit.getPluginCommand("stop").setExecutor(new restartServer());
     }
 }
